5f873f0f64961767365ea5bcd8a82acc5231779a
shuruikang <shuruimaths@gmail.com>
2024-02-27 15:27:27 +0100

    UPDATE:V1

--- src/train.py
+++ src/train.py
@@ -15,6 +15,23 @@ env = TimeLimit(
 # ENJOY!
 
 
+class QNetwork(nn.Module):
+    def __init__(self, state_dim, action_dim, hidden_size):
+        super(QNetwork, self).__init__()
+        self.fc1 = nn.Linear(state_dim, hidden_size)
+        self.fc2 = nn.Linear(hidden_size, hidden_size)
+        self.fc3 = nn.Linear(hidden_size, hidden_size)
+        self.fc4 = nn.Linear(hidden_size, hidden_size)
+        self.fc5 = nn.Linear(hidden_size, action_dim)
+
+    def forward(self, x):
+        x = torch.relu(self.fc1(x))
+        x = torch.relu(self.fc2(x))
+        x = torch.relu(self.fc3(x))
+        x = torch.relu(self.fc4(x))
+        return self.fc5(x)
+
+
 class ProjectAgent:
 
     def act(self, observation, use_random=False):
@@ -28,6 +45,7 @@ class ProjectAgent:
         pass
 
     def load(self):
+        self.model = QNetwork(6, 4, 256)
         checkpoint = torch.load("src/model.pt", map_location=torch.device('cuda' if torch.cuda.is_available() else 'cpu'))
         self.model.load_state_dict(checkpoint) 
         pass
