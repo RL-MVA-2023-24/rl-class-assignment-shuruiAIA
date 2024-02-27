d81d7e5e2d68763446da987b50a8bae233cd4593
shuruikang <shuruimaths@gmail.com>
2024-02-27 15:03:03 +0100

    UPDATE:V1

--- src/train.py
+++ src/train.py
@@ -2,6 +2,7 @@ from gymnasium.wrappers import TimeLimit
 from env_hiv import HIVPatient
 
 import torch
+import torch.nn as nn
 
 env = TimeLimit(
     env=HIVPatient(domain_randomization=False), max_episode_steps=200
@@ -13,6 +14,22 @@ env = TimeLimit(
 # Don't modify the methods names and signatures, but you can add methods.
 # ENJOY!
 
+class QNetwork(nn.Module):
+    def __init__(self, state_dim, action_dim, hidden_size):
+        super(QNetwork, self).__init__()
+
+        self.fc1 = nn.Linear(state_dim, hidden_size)
+        self.fc2 = nn.Linear(hidden_size, hidden_size)
+        self.fc3 = nn.Linear(hidden_size, hidden_size)
+        self.fc4 = nn.Linear(hidden_size, hidden_size)
+        self.fc5 = nn.Linear(hidden_size, action_dim)
+
+    def forward(self, x):
+        x = torch.relu(self.fc1(x))
+        x = torch.relu(self.fc2(x))
+        x = torch.relu(self.fc3(x))
+        x = torch.relu(self.fc4(x))
+        return self.fc5(x)
 
 class ProjectAgent:
 
@@ -27,6 +44,7 @@ class ProjectAgent:
         pass
 
     def load(self):
-        
-        self.model = torch.load("src/model.pt") 
+        self.model = QNetwork(6, 4, 256)
+        checkpoint = torch.load("src/model.pt", map_location=torch.device('cuda' if torch.cuda.is_available() else 'cpu'))
+        self.model.load_state_dict(checkpoint) 
         pass
